21535e9dec1a90508e8c537e9330fa422d4a0952
mateus.cmartins <mateus.cmartins@hotmail.com>
2023-04-12 22:23:24 -0300

    implementação da tabela na tela de lista de pts

---
+++ web-app/public/js/listaPT/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#tabela-pt').DataTable({
+      searching: false,
+      ordering:  true,
+      "paging": true,
+      "info": true, //determina a exibição de quantos itens aparecem
+      "dom": '<<t>ilp>', //determina posição dos elementos
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json'
+      }
+    });
+  });
