8a1f75caced4fd8148260570684f0a555c170075
mateus.cmartins <mateus.cmartins@hotmail.com>
2023-04-29 19:40:19 -0300

    desenvolvimento do endpoint da api do filtro de PT

--- api/src/controllers/permissaoDeTrabalho.js
+++ api/src/controllers/permissaoDeTrabalho.js
@@ -16,6 +16,49 @@ module.exports = {
     
         return res.json(permissoesDeTrabalho);
     
-    }
+    },
+
+    async buscarPTsFiltradas(req, res) {
+        
+        const {codigo_pt, status_pt, data_emissao, ordem_servico, emissor} = req.body;
+
+        const filtros = {};
+
+        if(codigo_pt){
+            filtros.codigo_pt = codigo_pt;
+        }
+
+        if(status_pt){
+            filtros.status_pt = status_pt;
+        }
+
+        if(data_emissao){
+            filtros.data_emissao = data_emissao;
+        }
+
+        if(ordem_servico){
+            filtros.codigo_os = ordem_servico;
+        }
+
+        if(emissor){
+            filtros.emissor = emissor;
+        }
+
+        console.log(filtros)
+        
+        const listaFiltrada = 
+            await connection('permissao_trabalho')
+            .select(
+                'permissao_trabalho.codigo_pt', 
+                'permissao_trabalho.status_pt',
+                'permissao_trabalho.ordem_servico as codigo_os', 
+                'ordem_servico.descricao')
+            .from('permissao_trabalho')
+            .leftJoin('ordem_servico', 'ordem_servico.ordem_servico', 'permissao_trabalho.ordem_servico')
+            .where(filtros)
+
+        return res.json(listaFiltrada);
+            
+    } 
 
 }
--- api/src/routes.js
+++ api/src/routes.js
@@ -24,8 +24,10 @@ routes.get('/usuario', UsuariosController.lerTodos);
 //routes.delete('/usuario/:matricula', UsuariosController.delete)
 
 //Rotas para lidar com as Permissões de Trabalho
-//  GET - busca todos os usuários
+//  GET - busca todas as permissões de trabalho no banco de dados
+//  POST - busca
 routes.get('/permissao_trabalho', PTController.lerTodos);
+routes.post('/permissao_trabalho/filtrada', PTController.buscarPTsFiltradas);
 
 
 //Exporta o modulo de rotas para ser utilizado por outros módulos
